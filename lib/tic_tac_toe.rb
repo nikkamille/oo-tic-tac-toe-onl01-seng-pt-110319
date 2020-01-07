95fde3bcc3738e73ee6551c4e1d96935a03af754
Kamille Longalong <k.pagibigan@gmail.com>
2020-01-07 09:05:01 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,7 +37,7 @@ class TicTacToe
   
   def valid_move?(index)
     # @board[index] == " "
-    index.between?(0, 8) && position_taken?(index)
+    index.between?(0, 8) && !position_taken?(index)
   end
   
   def turn 
